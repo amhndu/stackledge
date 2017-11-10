07c2342997f127bcf46b6c4ee840f5662332f2b8
Shivam Garg <gargshivam1040@gmail.com>
2017-11-11 00:22:51 +0530

    Header Added

---
+++ header/header_login.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Header login/register</title>
+
+    <link rel="stylesheet" href="assets/header-login-signup.css">
+    <link href='http://fonts.googleapis.com/css?family=Cookie' rel='stylesheet' type='text/css'>
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+  	<link href="css/header-login-register.css" rel="stylesheet">
+  	<link rel="stylesheet" href="css/font-awesome.css">
+
+  	<script src="js/jquery-1.10.2.js.download" type="text/javascript"></script>
+  	<script src="js/bootstrap.js.download" type="text/javascript"></script>
+  	<script src="js/login-register.js.download" type="text/javascript"></script>
+
+</head>
+<body class="" style="">
+  <header class="header-login-signup">
+  	<div class="header-limiter">
+  		<h1><a href="#">Stack<span>ledge</span></a></h1>
+  		<nav>
+  			<a href="#">Home</a>
+  			<a href="#" class="selected">Blog</a>
+  			<a href="#">Pricing</a>
+  		</nav>
+  		<ul>
+           <a href="javascript:void(0)" onclick="openLoginModal();">Log in</a>
+           |
+           <a data-toggle="modal" href="javascript:void(0)" onclick="openRegisterModal();">Register</a></div>
+  		</ul>
+  	</div>
+  </header>
+
+    <div class="container">
+		 <div class="modal fade login" id="loginModal" aria-hidden="true" style="display: none;">
+		      <div class="modal-dialog login animated">
+    		      <div class="modal-content">
+    		         <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                        <h4 class="modal-title">Login with</h4>
+                    </div>
+                    <div class="modal-body">
+                        <div class="box">
+                             <div class="content">
+                                <div class="error"></div>
+                                <div class="form loginBox">
+                                    <form method="post"  accept-charset="UTF-8" action = '/StackLedge/php/header.php'>
+                                    <input id="username" class="form-control" type="text" placeholder="Username" name="username">
+                                    <input id="password" class="form-control" type="password" placeholder="Password" name="password">
+                                    <input class="btn btn-default btn-login" type="submit" value="Login" >
+                                    </form>
+                                </div>
+                             </div>
+                        </div>
+                        <div class="box">
+                            <div class="content registerBox" style="display:none;">
+                             <div class="form">
+                                <form method="post" html="{:multipart=&gt;true}" data-remote="true" action="/StackLedge/php/user.php" accept-charset="UTF-8">
+                                <input id="username" class="form-control" type="text" placeholder="Username" name="username">
+                                <input id="email" class="form-control" type="text" placeholder="Email" name="email">
+                                <input id="password" class="form-control" type="password" placeholder="Password" name="password">
+                                <input id="password_confirmation" class="form-control" type="password" placeholder="Repeat Password" name="password_confirmation">
+                                <input type = 'hidden' name = 'newuser' value = 'true'>
+                                <input class="btn btn-default btn-register" type="submit" >
+                                </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <div class="forgot login-footer">
+                            <span>Looking to
+                                 <a href="javascript: showRegisterForm();">create an account</a>
+                            ?</span>
+                        </div>
+                        <div class="forgot register-footer" style="display:none">
+                             <span>Already have an account?</span>
+                             <a href="javascript: showLoginForm();">Login</a>
+                        </div>
+                    </div>
+    		      </div>
+		      </div>
+		  </div>
+    </div>
+<script type="text/javascript">
+    $(document).ready(function(){
+        openLoginModal();
+        <?php if(!$loginresult) echo 'shakeModal();'; ?>
+    });
+</script>
+</body></html>
---
+++ header/header_user.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Header login/register</title>
+
+    <link rel="stylesheet" href="assets/header-login-signup.css">
+    <link href='http://fonts.googleapis.com/css?family=Cookie' rel='stylesheet' type='text/css'>
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+  	<link href="css/header-login-register.css" rel="stylesheet">
+  	<link rel="stylesheet" href="css/font-awesome.css">
+
+  	<script src="js/jquery-1.10.2.js.download" type="text/javascript"></script>
+  	<script src="js/bootstrap.js.download" type="text/javascript"></script>
+  	<script src="js/login-register.js.download" type="text/javascript"></script>
+
+</head>
+<body class="" style="">
+  <header class="header-login-signup">
+  	<div class="header-limiter">
+  		<h1><a href="#">Stack<span>ledge</span></a></h1>
+  		<nav>
+  			<a href="#">Home</a>
+  			<a href="#" class="selected">Blog</a>
+  			<a href="#">Pricing</a>
+  		</nav>
+  		<ul>
+           <a href="javascript:void(0)" onclick="openLoginModal();"><?php echo $_SESSION['username']; ?></a>
+           |
+           <form method = 'post' style="display:inline-block;" action = '/StackLedge/php/header.php' id = 'form' >
+           <input type = 'hidden' name = 'logout' value = '1'>
+           <a href="" data-toggle="modal" onclick = 'document.getElementById("form").submit();'>Logout</a>
+           </form>
+           </div>
+  		</ul>
+  	</div>
+  </header>
+<?php //if(session_id()) print 'session started'; ?>
+</body></html>
