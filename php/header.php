f7538684158dc0ee3261bb28a5105c696a86101e
Shivam Garg <gargshivam1040@gmail.com>
2017-11-11 19:50:55 +0530

    Post Template update

--- php/header.php
+++ php/header.php
@@ -1,18 +1,18 @@
 <?php
 	if (session_status() == PHP_SESSION_NONE) {
-    session_start(); 
+    session_start();
 }
 ?>
 
 <!doctype html>
 <html>
     <head>
-        <title>Stackledge - The stack of knowledge</title>
-        <link rel="stylesheet" type="text/css" href="css/style.css">
-
-		<link rel="stylesheet" href="css/header-login-signup.css">
-		<link href='http://fonts.googleapis.com/css?family=Cookie' rel='stylesheet' type='text/css'>
-		<link href="css/bootstrap.css" rel="stylesheet">
+      <title>Stackledge - The stack of knowledge</title>
+			
+      <link rel="stylesheet" type="text/css" href="css/style.css">
+			<link rel="stylesheet" href="css/header-login-signup.css">
+			<link href='http://fonts.googleapis.com/css?family=Cookie' rel='stylesheet' type='text/css'>
+			<link href="css/bootstrap.css" rel="stylesheet">
 
 	  	<link href="css/header-login-register.css" rel="stylesheet">
 	  	<link rel="stylesheet" href="css/font-awesome.css">
@@ -21,7 +21,8 @@
 	  	<script src="js/jquery-1.10.2.js.download" type="text/javascript"></script>
 	  	<script src="js/bootstrap.js.download" type="text/javascript"></script>
 	  	<script src="js/login-register.js.download" type="text/javascript"></script>
-	  	
+			<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
     </head>
 <body>
 
--- templates/post.php
+++ templates/post.php
@@ -1,7 +1,6 @@
 <div class='card'>
   <div class='card_left'>
     <img src='https://mini.s-shot.ru/74x74/PNG/74/Z100/?<?php echo $post_url?>' width="74px" height="74px" />
-    <!-- http://www.google.com/s2/favicons?domain= -->
   </div>
   <div class='card_right'>
       <h1><a href="<?php echo $post_url?>"><?php echo $post_title?></a></h1>
@@ -11,10 +10,16 @@
         By <a href="user.php?u=<?php echo $post_owner?>"><?php echo $post_owner ?></a> in
         <a href="category.php?c=<?php echo $post_category?>"><?php echo $post_category ?></a>
         <br />
-        <a href=""><div class="upvote">
+        <a href="">
+          <div class="vote">
+            <i class="fa fa-caret-up fa-lg" aria-hidden="true"></i>
         </div></a>
         <?php echo $post_votes ?>
-        <a href=""><div class="downvote"></div></a>
+        <a href="">
+          <div class="vote">
+            <i class="fa fa-caret-down fa-lg" aria-hidden="true"></i>
+          </div>
+        </a>
         &nbsp;&nbsp; <a href=""><?php echo $post_num_comments ?> comments &nbsp;&nbsp; Add Comment</a>
         &nbsp;&nbsp; <?php echo $post_time?>
     </small>
