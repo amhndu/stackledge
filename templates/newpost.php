02c3d94857fed85f52894e6ded6f58bb710112aa
Shivam Garg <gargshivam1040@gmail.com>
2017-11-11 21:40:32 +0530

    Add new Post template Added

---
+++ templates/newpost.php
@@ -0,0 +1,19 @@
+<div class="newpost">
+  <form action="">
+
+    <label for="title">Title</label>
+    <input type="text" id="title" name="Title" placeholder="Title..">
+
+    <label for="url">URL</label>
+    <input type="text" id="url" name="lastname" placeholder="http://..">
+
+    <label for="category">Category</label>
+    <select name="category">
+		  <option value="foo">foo</option>
+		  <option value="bar">bar</option>
+		  <option value="test">test</option>
+		</select>
+
+    <input type="submit" value="Submit">
+  </form>
+</div>
