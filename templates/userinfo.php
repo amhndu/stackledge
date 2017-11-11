49297418cbaa295fb593741a3fa9ec0507e283a8
Shivam Garg <gargshivam1040@gmail.com>
2017-11-11 14:33:43 +0530

    UserInfo Template Added

---
+++ templates/userinfo.php
@@ -0,0 +1,28 @@
+<div class="user-container">
+    <div class="stats">
+        <div class="item linkkarma">
+            <div class="num letter1"><?php echo user_link?></div>
+            <div class="desc">Link <br />Reputation</div>
+        </div>
+        <div class="item commentkarma">
+            <div class="num letter1"><?php echo user_comment?></div>
+            <div class="desc">Comment Reputation</div>
+        </div>
+
+        <div class="item membertime">
+            <div class="num letter1"><?php echo user_age?></div>
+            <div class="desc">Months on StackLedge</div>
+        </div>
+    </div>
+
+    <div class="clear"></div>
+    <div class="user-contents">
+        <div class="column user-posts">
+            <div class="title">
+                User's Posts
+            </div>
+            <div class="contents"><div class="notfound">Posts not found</div></div>
+        </div>
+
+    </div>
+</div>
